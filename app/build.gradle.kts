5183705234ef437b92193b8def6b609b6a9ce22a
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-08-23 23:30:22 +0700

    Configure Speedometer Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ruzakj.speedometer"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.ruzakj.speedometer"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
